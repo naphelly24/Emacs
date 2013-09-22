d7ef2add28d21d1cab436f754e99152fd07cd21a
naphelly24 <naphelly24@126.com>
2013-09-22 15:43:38 +0800

    update

--- init-auto-complete.el
+++
@@ -1,8 +0,0 @@
-; seems el-get already set ac-dictionary-directories
-;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict") 
-(require 'auto-complete-config)
-(ac-config-default)
-(setq ac-quick-help-delay 0.5)      ; quick help show up after 0.5 seconds
-;(setq ac-auto-start nil)                ; do not auto auto-complete
-;(ac-set-trigger-key "TAB")              ; use TAB to trigger auto-complete
-(define-key ac-mode-map (kbd "TAB") 'auto-complete) ; use TAB to trigger auto-complete (fuzzy complete)
--- init-yasnippet.el
+++
@@ -1,3 +0,0 @@
-(require 'yasnippet)
-(setq yas/snippet-dirs '("~/.emacs.d/el-get/yasnippet/snippets" "~/.emacs.d/el-get/yasnippet/extras/imported"))
-(yas/global-mode 1)
